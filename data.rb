16c3b3bd9dd12847a507995caa0c1ab08a452970
Chris Erin and Jack Christensen <dev+chriserin+jackc@hashrocket.com>
2016-01-22 16:30:45 -0600

    Add data to tables

--- data.rb
+++ data.rb
@@ -1,3 +1,26 @@
 #!/usr/bin/ruby
 
+require 'bundler'
 
+Bundler.require
+
+DB = Sequel.postgres host: 'localhost', password: '123', user: 'dev', database: 'hr_hotels'
+
+DB.run('truncate sections restart identity cascade')
+
+def create_hotel
+  hotels = DB[:hotels]
+  hotels.insert
+end
+
+def create_section(hotel_id:, floor:, name:)
+  sections = DB[:sections]
+  sections.insert hotel_id: hotel_id, name: "Floor #{floor}", floor: floor
+end
+
+100.times do
+  hotel_id = create_hotel
+  5.times do |floor|
+    create_section(hotel_id: hotel_id, floor: floor, name: "Floor #{floor}")
+  end
+end
