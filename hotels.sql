38ea7b197a3b34876d809ec3a4a41ddb6012584f
Chris Erin and Josh Branchaud <dev+chriserin+jbranchaud@hashrocket.com>
2016-01-25 14:49:17 -0600

    Make foreign key columns not null

--- hotels.sql
+++ hotels.sql
@@ -14,29 +14,29 @@ create table hotels (
 
 -- TODO: Describe reasoning for creating sections better
 create table sections(
-  hotel_id integer references hotels,
+  hotel_id integer references hotels not null,
   id serial,
-  name varchar,
+  name varchar not null,
   prefix varchar not null default '',
   floor integer not null,
   primary key (hotel_id, id)
 );
 
 create table rooms (
-  hotel_id integer,
-  section_id integer,
+  hotel_id integer not null,
+  section_id integer not null,
   id serial,
-  name varchar,
+  name varchar not null,
   foreign key (hotel_id, section_id) references sections,
   primary key (hotel_id, section_id, id)
 );
 
 create table reservations (
   id serial primary key,
-  hotel_id integer,
-  section_id integer,
-  room_id integer,
-  days daterange,
+  hotel_id integer not null,
+  section_id integer not null,
+  room_id integer not null,
+  days daterange not null,
   foreign key (hotel_id, section_id, room_id) references rooms,
   constraint valid_days check (days <@ daterange('1900-01-01', '2100-01-01')), -- days must be within this range and additionally neither bound can be null
   exclude using gist (room_id with =, days with &&) -- exclude expression from being true between rows.  room_id must be unique for this daterange.
